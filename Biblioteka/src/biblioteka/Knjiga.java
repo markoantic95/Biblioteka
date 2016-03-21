74861d548be398b1819f21124230b5106cee7f33
markoantic95 <markoantic95@gmail.com>
2016-03-21 23:50:09 +0100

    Ubaceni atributi za klasu Knjiga

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -2,4 +2,40 @@ package biblioteka;
 
 public class Knjiga {
 
+		String naslov;
+		String isbn;
+		String autor;
+		String izdavac;
+		String izdanje;
+		public String getNaslov() {
+			return naslov;
+		}
+		public void setNaslov(String naslov) {
+			this.naslov = naslov;
+		}
+		public String getIsbn() {
+			return isbn;
+		}
+		public void setIsbn(String isbn) {
+			this.isbn = isbn;
+		}
+		public String getAutor() {
+			return autor;
+		}
+		public void setAutor(String autor) {
+			this.autor = autor;
+		}
+		public String getIzdavac() {
+			return izdavac;
+		}
+		public void setIzdavac(String izdavac) {
+			this.izdavac = izdavac;
+		}
+		public String getIzdanje() {
+			return izdanje;
+		}
+		public void setIzdanje(String izdanje) {
+			this.izdanje = izdanje;
+		}
+		
 }
